7df1fe5966ab09a1a88acd7065d500102e95d165
Mallory Porche <malloryburke@gmail.com>
2016-06-02 09:11:50 -0700

    Added ability to add addresses and phone numbers to contact via iGUI

--- address_book.rb
+++ address_book.rb
@@ -12,12 +12,19 @@ class AddressBook
 			puts "Address Book"
 			puts "a: Add Contact"
 			puts "p: Print Address Book"
+			puts "s: Search"
 			puts "e: Exit"
 			print "Enter your choice: "
 			input = gets.chomp.downcase
 			case input
 			when 'a' 
 				add_contact
+			when 's'
+				print "Search term:"
+				search = gets.chomp
+				find_by_name(search)
+				find_by_phone_number(search)
+				find_by_address(search)
 			when 'p'
 				print_contact_list
 			when 'e'
@@ -39,7 +46,6 @@ class AddressBook
 
 	def add_contact
 		contact = Contact.new
-
 		print "First Name:"
 		contact.first_name = gets.chomp
 		print "Middle Name:"
@@ -48,6 +54,41 @@ class AddressBook
 		contact.last_name = gets.chomp
 		contacts.push(contact)
 		print "Thank You. \n"
+
+		loop do 
+			puts "Add phone number or address?"
+			puts "p: Add phone number"
+			puts 'a: Add address'
+			puts "Any other key to go back"
+			response = gets.chomp.downcase
+			case response
+			when 'p'
+				phone = PhoneNumber.new
+				print "Phone number kind (Home, Work, etc): "
+				phone.kind = gets.chomp
+				print "Number: "
+				phone.number = gets.chomp
+				contact.phone_numbers.push(phone)
+			when 'a'
+				address = Address.new
+				print "Address Kind (Home, Work, etc): "
+				address.kind = gets.chomp
+				print "Address line 1: "
+				address.street_1 = gets.chomp
+				print "Address line 2: "
+				address.street_2 = gets.chomp
+				print "City: "
+				address.city = gets.chomp
+				print "State: "
+				address.state = gets.chomp
+				print "Postal Code: "
+				address.postal_code = gets.chomp
+				contact.addresses.push(address)
+			else
+				print "\n"
+				break
+			end
+		end
 	end
 
 	def find_by_name(name)
@@ -58,7 +99,7 @@ class AddressBook
 			results.push(contact)
 		 end
 		end
-		print_results("Name search results (#{search}", results)
+		print_results("Name search results #{search}", results)
 	end
 
 	def find_by_phone_number(number)
@@ -71,7 +112,7 @@ class AddressBook
 			end
 		end
 	end
-		print_results('Number search results (#{search}', results)
+		print_results("Number search results #{search}", results)
 	end
 
 	def find_by_address(query)
@@ -84,7 +125,7 @@ class AddressBook
 				end
 			end
 		end
-		print_results('Address search results #{search}', results)
+		print_results("Address search results #{search}", results)
 	end
 
 	def print_contact_list
